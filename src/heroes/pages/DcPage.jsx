f62fd8f7e7eaa3b1c1b6d2692d74ffc044af9dfb
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-15 22:13:43 -0300

    Agrega la lista de heroes en <DcPages />

--- src/heroes/pages/DcPage.jsx
+++ src/heroes/pages/DcPage.jsx
@@ -1,3 +1,12 @@
+import { HeroesList } from '../component/HeroesList';
+
 export const DcPage = () => {
-  return <h1>DcPage</h1>;
+  return (
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+
+      <HeroesList publisher={'DC Comics'} />
+    </>
+  );
 };
