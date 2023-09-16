376dd4b50cef9c14a726867bfa4f5d9b85e7e769
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-15 22:15:01 -0300

    Agrega la lista de heroes en <MarvelPages />

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -1,3 +1,12 @@
+import { HeroesList } from '../component/HeroesList';
+
 export const MarvelPage = () => {
-  return <h1>MarvelPage</h1>;
+  return (
+    <>
+      <h1>Dc Comics</h1>
+      <hr />
+
+      <HeroesList publisher={'Marvel Comics'} />
+    </>
+  );
 };
