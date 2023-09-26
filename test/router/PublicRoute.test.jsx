4e4d32fce68516dd9a1616b25cd0f0000956493c
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-26 11:20:34 -0300

    Pruebas en <PublicRoute />: debe navegar si está autenticado

--- test/router/PublicRoute.test.jsx
+++ test/router/PublicRoute.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import { AuthContext } from '../../src/auth';
 import { PublicRoute } from '../../src/router/PublicRoute';
+import { MemoryRouter } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 describe('Pruebas en <PublicRoute />', () => {
   test('debe mostrar el children si no está autenticado', () => {
@@ -17,5 +20,36 @@ describe('Pruebas en <PublicRoute />', () => {
     );
 
     expect(screen.getByText('Ruta pública')).toBeTruthy();
+  });
+
+  test('debe navegar si está autenticado', () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        name: 'Fer',
+        id: '1234',
+      },
+    };
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={['/login']}>
+          <Routes>
+            <Route
+              path="login"
+              element={
+                <PublicRoute>
+                  <h1>Ruta pública</h1>
+                </PublicRoute>
+              }
+            />
+
+            <Route path="marvel" element={<h1>Página de Marvel</h1>} />
+          </Routes>
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+
+    expect(screen.getByText('Página de Marvel')).toBeTruthy();
   });
 });
