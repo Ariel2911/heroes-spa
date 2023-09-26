20fa6206ca28db6ea5a9743b98af424060281928
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-26 10:25:00 -0300

    Pruebas en authReducer.js: debe llamar la ación login, pasar el logged a true y establecer el user

--- test/auth/context/authReducer.test.js
+++ test/auth/context/authReducer.test.js
@@ -1,19 +1,34 @@
 import { authReducer } from "../../../src/auth/context/authReducer"
+import { types } from "../../../src/auth/types/types";
 
 describe('Pruebas en authReducer', () => {
 
+  const user = {
+    id: 'ABC',
+    name: 'Ariel'
+  }
+
   const initialState = {
     logged: true,
-    user: {
-      id: 'ABC',
-      name,
-    }
+    user
   }
 
   test('debe retornar el estado por defecto', () => {
     const newState = authReducer(initialState,{});
 
     expect(newState).toBe(initialState);
-    
-  })
+
+  });
+
+  test('debe llamar la ación "login", pasar el logged a true y establecer el user', () => {
+    const action = {
+      type: types.login,
+      payload: user
+    };
+
+    const newState = authReducer({ logged: false }, action)
+
+    expect(newState).toEqual({ logged: true, user })
+
+  });
 })
